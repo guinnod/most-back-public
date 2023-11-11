16440016178367d5ac3b847883968ef918b28084
guinnod <210103064@stu.sdu.edu.kz>
2023-11-11 17:58:18 +0600

    feat: parse json body

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 5000;
+
+app.use(bodyParser());
 
 app.post("/", (req, res) => {
     console.log(req.body);
